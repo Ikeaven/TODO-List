a776a20277dc10f9b3ca7f3403e60f10d24d0443
Kevin Konrath <konrath.kevin@gmail.com>
2023-09-15 15:36:30 +0200

    close #7 : Styling

--- TODO-List/src/App.vue
+++ TODO-List/src/App.vue
@@ -12,6 +12,31 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+
+@use './assets/SCSS/base';
+@use './assets/SCSS/variables' as v;
+
+header{
+  background-color: v.$primaryColor-Light;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  nav{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    a{
+      color: v.$primaryColor;
+      font-size: 1.5rem;
+      font-weight: 700;
+      text-decoration: none;
+    }
+  }
+}
+
+
 
 </style>
--- TODO-List/src/components/TodoList.vue
+++ TODO-List/src/components/TodoList.vue
@@ -36,30 +36,92 @@ watch(todoList, (newValue) =>{
 </script>
 
 <template>
-    <form @submit.prevent="addTodo">
-        <input type="text" v-model="todo" placeholder="Add a new todo">
-        <button type="submit">Add</button>
-    </form>
-
-    <div v-for="todo in todoList" >
-        <div class="todo_wrapper">
+    <div class="todolist_wrapper">
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="todo" placeholder="Add a new todo">
+            <button type="submit">Add</button>
+        </form>
+    
+        
+        <div v-for="todo in todoList" class="todo_wrapper">
             <img @click="deleteTodo(todo.id)" src="../assets/img/trash-can-solid.svg" alt="trash icon" class="todo_trashIcon">
             <input type="checkbox" v-model="todo.completed" class="todo_checkbox">
-            <input type="text" v-model="todo.title" :class="`${todo.completed ? 'completed': ''}`">
+            <input type="text" v-model="todo.title" :class="`todo_title ${todo.completed ? 'completed': ''}`">
         </div>
     </div>
-
 </template>
 
 <style scoper lang="scss">
-.completed{
-    text-decoration: line-through;
+
+@use '../assets/SCSS/variables' as v;
+
+
+
+.todolist_wrapper{
+    width: 100%;
+}
+
+form{
+
+    padding-left: 20px;
+    display: flex;
+    align-items: center;
+    margin: 20px 0;
+    input{
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        outline: none;
+        background-color: v.$primaryColor-Light;
+        box-shadow: none;
+        color: v.$primaryColor;
+        font-size: 1.2rem;
+    }
+    input:focus{
+        outline: none;
+    }
+    button{
+        margin-left: 10px;
+        border-radius: 5px;
+        border: none;
+        outline: none;
+        background-color: v.$primaryColor-Light;
+        box-shadow: none;
+        color: v.$primaryColor;
+        font-size: 1.2rem;
+        padding: 10px;
+        cursor: pointer;
+    }
+
 }
 
 .todo_wrapper{
     display: flex;
     align-items: center;
+    // justify-content: center;
     margin: 10px 0;
+    padding-left: 20px;
+    width: 100%;
+
+}
+
+.todo_title{
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    box-shadow: none;
+    color: v.$primaryColor-Light;
+    font-size: 1.2rem;
+    text-overflow: ellipsis;
+    overflow-wrap: break-word;
+    white-space: normal;
+    width: 100%;
+}
+
+.completed{
+    text-decoration: line-through v.$dangerColor 3px;
 }
 
 .todo_trashIcon{
@@ -70,5 +132,7 @@ watch(todoList, (newValue) =>{
 .todo_checkbox{
     margin: 0 10px;
     cursor: pointer;
+    accent-color: v.$primaryColor-Light;
 }
+
 </style>
