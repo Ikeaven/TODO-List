41aeb9b0fdf4ffa598df8974d37f552373d7627b
Kevin Konrath <konrath.kevin@gmail.com>
2023-09-15 14:00:00 +0200

    close #5; DELETE TASK

--- TODO-List/src/components/TodoList.vue
+++ TODO-List/src/components/TodoList.vue
@@ -17,6 +17,11 @@ function addTodo() {
     todo.value = ''
 }
 
+function deleteTodo(id){
+    todoList.value = todoList.value.filter(todo => todo.id !== id)
+}
+
+
 onBeforeMount(() => {
     const todos = localStorage.getItem('todoList') || []
     if(todos.length > 0){
@@ -37,16 +42,33 @@ watch(todoList, (newValue) =>{
     </form>
 
     <div v-for="todo in todoList" >
-        <input type="checkbox" v-model="todo.completed">
-        <input type="text" v-model="todo.title" :class="`${todo.completed ? 'completed': ''}`">
+        <div class="todo_wrapper">
+            <img @click="deleteTodo(todo.id)" src="../assets/img/trash-can-solid.svg" alt="trash icon" class="todo_trashIcon">
+            <input type="checkbox" v-model="todo.completed" class="todo_checkbox">
+            <input type="text" v-model="todo.title" :class="`${todo.completed ? 'completed': ''}`">
+        </div>
     </div>
 
 </template>
 
-<style lang="scss">
+<style scoper lang="scss">
 .completed{
     text-decoration: line-through;
 }
 
+.todo_wrapper{
+    display: flex;
+    align-items: center;
+    margin: 10px 0;
+}
 
+.todo_trashIcon{
+    width: 15px;
+    cursor: pointer;
+}
+
+.todo_checkbox{
+    margin: 0 10px;
+    cursor: pointer;
+}
 </style>
