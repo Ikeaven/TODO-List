80d08f370a862b3ca1112c32f5e2015fec8fc0a1
Kevin Konrath <konrath.kevin@gmail.com>
2023-09-15 12:58:25 +0200

    close #1 : PROJECT SETUP

---
+++ TODO-List/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Todo list</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
