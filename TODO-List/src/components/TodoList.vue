677054029b2ce7fddbfd0656369e895049c36756
Kevin Konrath <konrath.kevin@gmail.com>
2023-09-15 13:09:43 +0200

    close #4 : ADD TASK

--- TODO-List/src/components/TodoList.vue
+++ TODO-List/src/components/TodoList.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+
+const todo = ref('')
+const todoList = ref([])
+
+function addTodo() {
+    if(todo.value.trim() === ''){
+        return
+    }
+    todoList.value.push(todo.value)
+    todo.value = ''
+}
 
 </script>
 
 <template>
-    
+    <form @submit.prevent="addTodo">
+        <input type="text" v-model="todo" placeholder="Add a new todo">
+        <button type="submit">Add</button>
+    </form>
 </template>
 
 <style scoped>
